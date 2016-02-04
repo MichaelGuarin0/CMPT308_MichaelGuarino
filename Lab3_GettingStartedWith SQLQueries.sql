4d9696cfff7a465419f767bf4b356d1406536911
Mike <Michael@Michaels-MBP-2.fios-router.home>
2016-02-04 15:30:18 -0500

    Lab 3_Completed Tests on Answer 2
    Answer to question2 Correct when tested

--- Lab3_GettingStartedWith SQLQueries.sql
+++ Lab3_GettingStartedWith SQLQueries.sql
@@ -8,54 +8,46 @@
 	SELECT ordnum, totalUSD
 	FROM orders;
 
--- 2. List the name and city of agents named Smith.
+-- TESTED_2. List the name and city of agents named Smith.
 	SELECT name, city
 	FROM agents
 	WHERE name='Smith';
 
--- 3. List the pid, name, and priceUSD of products with quantity more than 208,000.
+-- unTESTED_3. List the pid, name, and priceUSD of products with quantity more than 208,000.
+	SELECT pid, name, priceUSD
+	FROM products
+	WHERE quantity > 208000;
 
--- 4. List the names and cities of customers in Dallas.
+-- unTESTED_4. List the names and cities of customers in Dallas.
+	SELECT name, city
+	FROM customers
+	WHERE city='Dallas';
 
--- 5. List the names of agents not in New York and not in Tokyo.
+-- unTESTED_5. List the names of agents not in New York and not in Tokyo.
+	SELECT name
+	FROM agents
+	WHERE city !='New York'
+		AND city != 'Tokyo';
 
--- 6. List all data for products not in Dallas or Duluth that cost US$1 or more.
+-- unTESTED_6. List all data for products not in Dallas or Duluth that cost US$1 or more.
+	SELECT *
+	FROM products
+	WHERE (city != 'Dallas' OR city != 'Duluth')
+		AND (priceUSD >= '1.00'); 
 
--- 7. List all data for orders in January or March.
+-- unTESTED_7. List all data for orders in January or March.
 	SELECT *
 	FROM orders
 	WHERE mon='jan'
 		OR mon='mar';
 
--- 8. List all data for orders in February less than us$500.
--- 9. List all orders from the customer whose cid is C005.
-
-
-
-SELECT *
+-- unTESTED_8. List all data for orders in February less than us$500.
+	SELECT *
 	FROM orders
-	WHERE mon IN ('jan', 'mar');
+	WHERE mon='feb'
+		AND totalUSD < 500.00;
 
---impose order on the output
-SELECT *
-FROM orders
-WHERE mon IN ('jan', 'mar');
-ORDER BY totalUSD DESC
-
-SELECT *
-FROM orders
-WHERE mon IN ('jan', 'mar');
-ORDER BY totalUSD ASC
-
---can you order on data that is not displayed
-SELECT totalUSD, ordnum, mon
-FROM orders
-WHERE mon IN ('jan', 'mar');
-ORDER BY totalUSD ASC, ordnum DESC
-
---with these two examples there is something special with unions and something else
-SELECT ALL totalUSD
-FROM orders
-
-SELECT DISTINCT totalUSD
-FROM orders
+-- unTESTED_9. List all orders from the customer whose cid is C005.
+	SELECT *
+	FROM orders
+	WHERE cid='C005';
