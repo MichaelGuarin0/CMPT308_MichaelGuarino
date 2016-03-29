a20e4096b9aab9b86205f2a402719790026a4f29
MichaelGuarin0 <michael.guarino1@marist.edu>
2016-03-29 10:29:00 -0400

    Lab7_NormalizationOne

--- Lab5_SQL Queries_TheJoinsThreeQuel.sql
+++ Lab5_SQL Queries_TheJoinsThreeQuel.sql
@@ -16,6 +16,30 @@ WHERE customers.city='Dallas'
 GROUP BY pid
 HAVING count(orders.aid)>=1
 ORDER BY pid DESC; 
+-- correct answer subquery:
+SELECT distinct o.aid
+FROM orders o
+WHERE aid IN(SELECT distinct o.aid
+			from customers c,
+			orders o
+			WHERE o.cid=c.cid
+			and c.city='Dallas')
+order by pid DESC;
+-- correct answer joins:
+select distinct o2.pid 
+from customers c, 
+	orders o1
+	orders o2
+where o1.cid=c.cid
+	and o1.aid=o2.aid
+	and c.city='Dallas'
+order by o2.pid desc;
+-- correct answer inner join syntax:
+select distinct o2.pid
+from customers c inner join orders o1 on c.cid=o1.cid
+				inner join orders o2 on o1.aid=o2.aid
+where c.city='Dallas'
+order by o2.pid desc
 
 -- 3) TESTED_WORKS: Show the names of customers who have never placed an order. Use subquery.
 SELECT name
@@ -30,9 +54,10 @@ WHERE cid=(SELECT cid
 SELECT name
 FROM customers LEFT OUTER JOIN orders ON customers.cid=orders.cid
 WHERE orders.cid is NULL;
+-- using null here rather than not in speeds up query 
 
 -- 5) TESTED_WORKS: Show the names of customers who placed at least one order through an agent in their own city, along with those agents names.
-SELECT customers.name, agents.name 
+SELECT distinct customers.name, agents.name 
 FROM customers INNER JOIN agents ON customers.city=agents.city
 INNER JOIN orders ON customers.cid=orders.cid AND agents.aid=orders.aid;
 
@@ -45,3 +70,14 @@ SELECT products.city, customers.name, count(products.city) AS Num_of_Products
 FROM products INNER JOIN customers ON products.city=customers.city 
 GROUP BY products.city, customers.name
 ORDER BY products.city DESC LIMIT 1;
+
+-- alan's better looking solution:
+select name, city
+from customers
+where city in(select city
+				from(select p.city, count(*)
+					from products p
+						group by p.city
+						order by count(*) asc 
+						limit 1) sub1
+				)
